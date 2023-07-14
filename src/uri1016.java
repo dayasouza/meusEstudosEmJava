868495688cee6773f7982c36748fcb7af5bcfbd9
dayasouza <dayanalvesouza@gmail.com>
2023-07-13 21:20:34 -0300

    Resolvido exercicio uri1016

--- src/uri1016.java
+++ src/uri1016.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class uri1016 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int dist, tempo;
+        int veloX = 60;
+        int vloY = 90;
+
+        dist = sc.nextInt();
+
+        tempo = dist * 2;
+
+        System.out.println(tempo + " minutos");
+        sc.close();
+    }
 }
