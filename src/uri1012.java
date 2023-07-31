35c8e81a5d2f51e681fb19db8f70425ecf53436e
dayasouza <dayanalvesouza@gmail.com>
2023-07-31 20:50:57 -0300

    Resolvido os exercicios uri1011 uri1012 uri1013 e uri1015

--- src/uri1012.java
+++ src/uri1012.java
@@ -1,2 +1,43 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class uri1012 {
+    /*
+    Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e
+    mostre:
+    a) a área do triângulo retângulo que tem A por base e C por altura.
+    b) a área do círculo de raio C. (pi = 3.14159)
+    c) a área do trapézio que tem A e B por bases e C por altura.
+    d) a área do quadrado que tem lado B.
+    e) a área do retângulo que tem lados A e B.
+    Entrada: O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+    Saída: O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a uma das áreas descritas acima,
+    sempre com mensagem correspondente e um espaço entre os dois pontos e o valor. O valor calculado deve ser
+    apresentado com 3 dígitos após o ponto decimal.
+     */
+
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double a, b, c;
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = sc.nextDouble();
+
+        double triangulo = (a * c) / 2;
+        double circulo = 3.14159 * (c * c);
+        double trapezio = ((a + b) * c) / 2;
+        double quadrado = b * b;
+        double retangulo = a * b;
+
+        System.out.printf("TRIANGULO: %.3f\n", triangulo);
+        System.out.printf("CIRCULO: %.3f\n", circulo);
+        System.out.printf("TRAPEZIO: %.3f\n", trapezio);
+        System.out.printf("QUADRADO: %.3f\n", quadrado);
+        System.out.printf("RETANGULO: %.3f\n", retangulo);
+
+
+        sc.close();
+    }
 }
--- src/uri1015.java
+++ src/uri1015.java
@@ -1,2 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class uri1015 {
+
+    /*
+    Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e
+    calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+
+    Distancia = raiz quadradada de (x2 -x1)² + (y2 - y1)²
+
+    Entrada: O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de
+    ponto flutuante: x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
+
+    Saída: Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+     */
+
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        float p1, x1, y1, p2, x2, y2, distancia;
+        x1 = sc.nextFloat();
+        y1 = sc.nextFloat();
+        x2 = sc.nextFloat();
+        y2 = sc.nextFloat();
+
+        p1 = (x2 - x1) * (x2 - x1);
+        p2 = (y2 - y1) * (y2 - y1);
+
+        distancia = (float) Math.sqrt(p1 + p2);
+
+        System.out.printf("%.4f\n", distancia);
+
+        sc.close();
+    }
 }
