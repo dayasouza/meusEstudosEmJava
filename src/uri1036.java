27c7247439ef953511c6ef5df8e6a6db45f71bf4
dayasouza <dayanalvesouza@gmail.com>
2023-08-03 20:39:46 -0300

    Adicionado exercicios resolvidos de estruturacondiconal

---
+++ src/uri1036.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1036 {
+    public static void main(String[] args){
+        /*
+        Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível
+        calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou
+        raiz de numero negativo.
+        Entrada: Leia três valores de ponto flutuante (double) A, B e C.
+        Saída: Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular".
+        Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente
+        conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
+         */
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double delta = b * b - 4.0 * a * c;
+
+        if (a == 0 || delta < 0.0){
+            System.out.println("Impossivel calcular");
+        } else {
+            double r1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+            double r2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+            System.out.printf("R1 = %.5f\n" , r1);
+            System.out.printf("R2 = %.5f\n", r2);
+        }
+
+        sc.close();
+    }
+}
